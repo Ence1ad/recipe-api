43aac788a71f37370e51b19ded0f132e833d5c47
Encelad <fantminer@gmail.com>
2024-03-19 19:57:39 +0300

    Add filter for tags and ingredients

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -340,6 +340,46 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(recipe.ingredients.count(), 0)
 
+    def test_filter_by_tags(self):
+        """Test filtering recipes by tags."""
+        r1 = create_recipe(user=self.user, title='Borsh')
+        r2 = create_recipe(user=self.user, title='Okroshka')
+        tag1 = Tag.objects.create(user=self.user, name='Vegan')
+        tag2 = Tag.objects.create(user=self.user, name='Vegetarian')
+        r1.tags.add(tag1)
+        r2.tags.add(tag2)
+        r3 = create_recipe(user=self.user, title='Uha')
+
+        params = {'tags': f'{tag1.id}, {tag2.id}'}
+        res = self.client.get(RECIPES_URL, params)
+
+        s1 = RecipeSerializer(r1)
+        s2 = RecipeSerializer(r2)
+        s3 = RecipeSerializer(r3)
+        self.assertIn(s1.data, res.data)
+        self.assertIn(s2.data, res.data)
+        self.assertNotIn(s3.data, res.data)
+
+    def test_filter_by_ingredients(self):
+        """Test filtering by ingredients."""
+
+        r1 = create_recipe(user=self.user, title='Chebureks')
+        r2 = create_recipe(user=self.user, title='Pirogki')
+        ing1 = Ingredient.objects.create(user=self.user, name='Mince')
+        ing2 = Ingredient.objects.create(user=self.user, name='Cabbage')
+        r1.ingredients.add(ing1)
+        r2.ingredients.add(ing2)
+        r3 = create_recipe(user=self.user, title='Pancakes')
+
+        params = {'ingredients': f'{ing1.id}, {ing2.id}'}
+        res =self.client.get(RECIPES_URL, params)
+        s1 = RecipeSerializer(r1)
+        s2 = RecipeSerializer(r2)
+        s3 = RecipeSerializer(r3)
+        self.assertIn(s1.data, res.data)
+        self.assertIn(s2.data, res.data)
+        self.assertNotIn(s3.data, res.data)
+
 class ImageUploadTests(TestCase):
     """Tests for the image upload API."""
     def setUp(self):
